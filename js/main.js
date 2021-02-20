6002a1ca2ae6e9b09e8ddda7b36edd7da5bc812d
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-20 22:16:31 +0100

    new 2 sliders

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
-$('.FairyTail_slider').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
+$(document).ready(function(){
+    $('.Slider').slick({
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
   });
