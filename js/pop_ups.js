120b148c6e3f5386b8bac48beb4820f2bcb55cd2
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-02-22 23:04:44 +0100

    added_fwoo_popups

--- js/pop_ups.js
+++ js/pop_ups.js
@@ -3,14 +3,20 @@ var pop_up1 = document.getElementsByClassName("Pop-up First")[0];
 var pop_up2 = document.getElementsByClassName("Pop-up Second")[0];
 var pop_up3 = document.getElementsByClassName("Pop-up Threeth")[0];
 var pop_up4 = document.getElementsByClassName("Pop-up Fourth")[0];
+var pop_up5 = document.getElementsByClassName("Pop-up Fifth")[0];
+var pop_up6 = document.getElementsByClassName("Pop-up Sixth")[0];
 var btn = document.getElementsByClassName("blok first")[0];
 var btn2 = document.getElementsByClassName("blok second")[0];
 var btn3 = document.getElementsByClassName("blok threeth")[0];
 var btn4 = document.getElementsByClassName("blok fourth")[0];
+var btn5 = document.getElementsByClassName("blok fifth")[0];
+var btn6 = document.getElementsByClassName("blok sixth")[0];
 var CloseButton = document.getElementsByClassName("ClosePop-upHotel")[0];
 var CloseButton2 = document.getElementsByClassName("ClosePop-upHotel Second")[0];
 var CloseButton3 = document.getElementsByClassName("ClosePop-upHotel Threeth")[0];
 var CloseButton4 = document.getElementsByClassName("ClosePop-upHotel Fourth")[0];
+var CloseButton5 = document.getElementsByClassName("ClosePop-upHotel Fifth")[0];
+var CloseButton6 = document.getElementsByClassName("ClosePop-upHotel Sixth")[0];
 
 btn.onclick = function() {
     pop_up1.style.display = "block"
@@ -24,6 +30,12 @@ btn3.onclick = function() {
 btn4.onclick = function() {
     pop_up4.style.display = "block"
 }
+btn5.onclick = function() {
+    pop_up5.style.display = "block"
+}
+btn6.onclick = function() {
+    pop_up6.style.display = "block"
+}
 
 CloseButton.onclick = function() {
     pop_up1.style.display = "none"
@@ -36,6 +48,12 @@ CloseButton3.onclick = function() {
 }
 CloseButton4.onclick = function() {
     pop_up4.style.display = "none"
+}
+CloseButton5.onclick = function() {
+    pop_up5.style.display = "none"
+}
+CloseButton6.onclick = function() {
+    pop_up6.style.display = "none"
 }
 
 
