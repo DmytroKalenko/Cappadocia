2f0a500dbd31835904df2269ad2286010f8c2b85
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-02-27 14:42:36 +0100

    addBurgerMenu

--- js/main.js
+++ js/main.js
@@ -8,44 +8,53 @@ $(document).ready(function() {
         dots: true,
 
     });
+    //bURGER MENU
+    $('.header__burger , .header__list li a').on('click', function() {
+        $('.header__list').slideToggle();
+    });
+    $('.header__burger').on('click', function() {
+        $('.header__burger').toggleClass("Action");
 
-
-    new fullpage('#fullPage', {
-        scrollingSpeed: 900,
-        parallax:true,
-        anchors: ['page1', 'page2', 'page3', 'page4','page5', 'page6', 'page7', 'page8' ],
     });
 
+    //Sliders_in_PopUps
     $('.pop_up__slider').slick({
         autoplay: true,
         infinite: true,
         speed: 500,
     });
-    //adding the action to the button
-    $(document).on('click', '#UPButton', function(){
+
+    //ScrollPAge
+    new fullpage('#fullPage', {
+        scrollingSpeed: 900,
+        parallax: true,
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7', 'page8'],
+    });
+    //adding the action to ScrollPAge
+    $(document).on('click', '#UPButton', function() {
         fullpage_api.moveTo('page1');
-      });
-$(document).on('click', '#Our_trip', function(){
-    fullpage_api.moveTo('page3', 1);
-  });
-  $(document).on('click', '#FairyTail', function(){
-    fullpage_api.moveTo('page2', 1);
-  });
-  $(document).on('click', '#Hotel', function(){
-    fullpage_api.moveTo('page4', 1);
-  });
-  $(document).on('click', '#Attractions', function(){
-    fullpage_api.moveTo('page5', 1);
-  });
-  $(document).on('click', '#Price', function(){
-    fullpage_api.moveTo('page6', 1);
-  });
-  $(document).on('click', '#BookNow', function(){
-    fullpage_api.moveTo('page7', 1);
-  });
-  $(document).on('click', '#Contacts', function(){
-    fullpage_api.moveTo('page8', 1);
-  });
- 
+    });
+    $(document).on('click', '#Our_trip', function() {
+        fullpage_api.moveTo('page3', 1);
+    });
+    $(document).on('click', '#FairyTail', function() {
+        fullpage_api.moveTo('page2', 1);
+    });
+    $(document).on('click', '#Hotel', function() {
+        fullpage_api.moveTo('page4', 1);
+    });
+    $(document).on('click', '#Attractions', function() {
+        fullpage_api.moveTo('page5', 1);
+    });
+    $(document).on('click', '#Price', function() {
+        fullpage_api.moveTo('page6', 1);
+    });
+    $(document).on('click', '#BookNow', function() {
+        fullpage_api.moveTo('page7', 1);
+    });
+    $(document).on('click', '#Contacts', function() {
+        fullpage_api.moveTo('page8', 1);
+    });
+
 
 });
