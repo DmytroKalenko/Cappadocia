de95a7d94d1de5ba8a5c68afac1a49e0072cecb4
DmytroKalenko <DmytroKalenko@users.noreply.github.com>
2021-02-27 11:44:55 +0100

    addScroll

--- js/main.js
+++ js/main.js
@@ -11,7 +11,9 @@ $(document).ready(function() {
 
 
     new fullpage('#fullPage', {
-        scrollingSpeed: 500,
+        scrollingSpeed: 900,
+        parallax:true,
+        anchors: ['page1', 'page2', 'page3', 'page4','page5', 'page6', 'page7', 'page8' ],
     });
 
     $('.pop_up__slider').slick({
@@ -19,5 +21,31 @@ $(document).ready(function() {
         infinite: true,
         speed: 500,
     });
+    //adding the action to the button
+    $(document).on('click', '#UPButton', function(){
+        fullpage_api.moveTo('page1');
+      });
+$(document).on('click', '#Our_trip', function(){
+    fullpage_api.moveTo('page3', 1);
+  });
+  $(document).on('click', '#FairyTail', function(){
+    fullpage_api.moveTo('page2', 1);
+  });
+  $(document).on('click', '#Hotel', function(){
+    fullpage_api.moveTo('page4', 1);
+  });
+  $(document).on('click', '#Attractions', function(){
+    fullpage_api.moveTo('page5', 1);
+  });
+  $(document).on('click', '#Price', function(){
+    fullpage_api.moveTo('page6', 1);
+  });
+  $(document).on('click', '#BookNow', function(){
+    fullpage_api.moveTo('page7', 1);
+  });
+  $(document).on('click', '#Contacts', function(){
+    fullpage_api.moveTo('page8', 1);
+  });
+ 
 
 });
