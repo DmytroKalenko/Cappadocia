bde1d2fffe71e9c53fa9ef06b0b1d9abd17a4e93
DmytroKalenko <76845911+DmytroKalenko@users.noreply.github.com>
2021-04-07 20:21:34 +0200

    new_change

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-$(document).ready(function () {
-    
-   //Sliders
+$(document).ready(function() {
+
+    //Sliders
     $('.Main_Slider').slick({
         autoplay: true,
         infinite: true,
@@ -20,8 +20,8 @@ $(document).ready(function () {
         fade: true,
         cssEase: 'linear',
         dots: false,
-       
-       
+
+
     });
     $('.OurTrip_Slider').slick({
         autoplay: true,
@@ -66,129 +66,140 @@ $(document).ready(function () {
         }]
     });
 
-    
+
     //bURGER MENU
-    $('.header__burger, .header__list ul li').on('click', function () {
+    $('.header__burger, .header__list ul li').on('click', function() {
         $('.header__burger, .header__list').toggleClass("Action");
     });
 
     //Pup-ups in Hotel Slide 
     //first   
-    $('.blok.first').on('click', function () {
+    $('.blok.first').on('click', function() {
         $('.Pop_up.First').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up').on('click', function () {
+    $('.ClosePop-up').on('click', function() {
         $('.Pop_up.First').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
     //Second
-    $('.blok.second').on('click', function () {
+    $('.blok.second').on('click', function() {
         $('.Pop_up.Second').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up.Second').on('click', function () {
+    $('.ClosePop-up.Second').on('click', function() {
         $('.Pop_up.Second').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
-  
+
     //Threeth
-    $('.blok.threeth').on('click', function () {
+    $('.blok.threeth').on('click', function() {
         $('.Pop_up.Threeth').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up.Threeth').on('click', function () {
+    $('.ClosePop-up.Threeth').on('click', function() {
         $('.Pop_up.Threeth').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
 
     //Fourth
-    $('.blok.fourth').on('click', function () {
+    $('.blok.fourth').on('click', function() {
         $('.Pop_up.Fourth').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up.Fourth').on('click', function () {
+    $('.ClosePop-up.Fourth').on('click', function() {
         $('.Pop_up.Fourth').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
 
     //Fifth
-    $('.blok.fifth').on('click', function () {
+    $('.blok.fifth').on('click', function() {
         $('.Pop_up.Fifth').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up.Fifth').on('click', function () {
+    $('.ClosePop-up.Fifth').on('click', function() {
         $('.Pop_up.Fifth').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
-    
+
     //Sixth
-    $('.blok.sixth').on('click', function () {
+    $('.blok.sixth').on('click', function() {
         $('.Pop_up.Sixth').toggleClass("Active");
         $('body').toggleClass("NOScroll");
         $('.Pop_up__slider').slick('setPosition');
     });
 
-    $('.ClosePop-up.Sixth').on('click', function () {
+    $('.ClosePop-up.Sixth').on('click', function() {
         $('.Pop_up.Sixth').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
 
     //Book now
-    $('.OpenPop_upBtn').on('click', function () {
+    $('.OpenPop_upBtn').on('click', function() {
         $('.Pop_up.Book').toggleClass("Active");
         $('body').toggleClass("NOScroll");
-    
+
     });
 
-    $('.ClosePop-up.Sixth').on('click', function () {
+    $('.ClosePop-up.Sixth').on('click', function() {
         $('.Pop_up.Book').removeClass("Active");
         $('body').removeClass("NOScroll");
     });
     //lazyLoad
     var lazyLoadInstance = new LazyLoad({
         // Your custom settings go here
-      });
+    });
 
     //BURGER___MENU
-    $('.burger').click(function(){
+    $('.burger').click(function() {
         $('.burger').toggleClass("Active");
         $('.menu').toggleClass("Active");
         $('body').toggleClass("NOScroll");
     });
 
-    $('.menu ul li').click(function(){
-         $('body').removeClass("NOScroll");
+    $('.menu ul li').click(function() {
+        $('body').removeClass("NOScroll");
         $('.burger, .menu').removeClass("Active");
     })
-    $('.blok, .OpenPop_upBtn').click(function(){
+    $('.blok, .OpenPop_upBtn').click(function() {
         $(".burger").fadeOut()
     })
-    $('.ClosePop-up').click(function(){
+    $('.ClosePop-up').click(function() {
         $(".burger").fadeIn()
     })
 
     ///Add__Button__UP
     var UPbtn = $('.UPButton')
-    $(window).on("scroll",function(){
-       if ($(this).scrollTop() >= 20){
-           UPbtn.fadeIn();
-       } else{
-        UPbtn.fadeOut();
-       }
+    $(window).on("scroll", function() {
+        if ($(this).scrollTop() >= 20) {
+            UPbtn.fadeIn();
+        } else {
+            UPbtn.fadeOut();
+        }
     })
-    UPbtn.click(function(){
-        $("html, body").animate({scrollTop:0}, 100)
-    })
+    UPbtn.click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 100)
+    });
+
+    ///Add_backgroundcolot_to_top_nav
+
+    // const TopMenu = document.querySelector('.menu__backgrount__blok');
+    // $(window).on("scroll", function() {
+    //     if ($(this).scrollTop() >= 20) {
+    //         TopMenu.fadeIn();
+    //     } else {
+    //         TopMenu.fadeOut();
+    //     }
+    // });
 
 });
